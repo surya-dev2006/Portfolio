18a0f96c16647c23aaea3dbe10b929190a0cdfaa
surya-dev2006 <surya277728@gmail.com>
2026-05-07 15:01:01 +0530

    hekko

--- Portfolio/src/Components/Languages.jsx
+++ Portfolio/src/Components/Languages.jsx
@@ -4,24 +4,7 @@ const Languages = () => {
   return (
     <div>
         <div className="skill-container d-flex ">
-      <div className="skilll">
-       
-        <div className='d-flex flex-column justify-content-center align-items-center '>
-           <img className='l-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
-            <p className='l-name'>HTML</p>
-        </div>
-        
-      </div>
-         <div className="skilll">
-       
-        <div className='d-flex flex-column justify-content-center align-items-center '>
-           
-            <img className='l-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
-          
-            <p className='l-name'>CSS</p>
-        </div>
-        
-      </div>
+
          <div className="skilll">
        
         <div className='d-flex flex-column justify-content-center align-items-center '>
@@ -30,7 +13,7 @@ const Languages = () => {
             <img className='l-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
           
           
-            <p className='l-name'>JAVA SCRIPT</p>
+            <p style={{fontSize:"14px"}} className='l-name'>Web Development</p>
         </div>
         
       </div>
